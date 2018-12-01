9a499d0060b304cd3ca483d523c1689c324e808f
Alexander <praddcell@gmail.com>
2018-12-01 14:57:58 +0300

    6 10 cascade ddl

--- src/test/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepositoryImpl.java
+++ src/test/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepositoryImpl.java
@@ -2,7 +2,7 @@ package ru.javawebinar.topjava.repository.inmemory;
 
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.UserTestData;
-import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.model .User;
 import ru.javawebinar.topjava.repository.UserRepository;
 
 import java.util.Comparator;
